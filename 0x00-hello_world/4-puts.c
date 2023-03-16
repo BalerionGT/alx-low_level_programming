ee4c1611bb0e55b86fcdf7bf79a5572166aac95d
root <elafouibadr@gmail.com>
2023-03-16 15:58:35 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ * description: this program print a sentence
+ * Return: always 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
