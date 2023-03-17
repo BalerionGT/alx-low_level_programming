080e690069952e9d3a5919e7e26ca94af5c86e9e
root <elafouibadr@gmail.com>
2023-03-16 18:03:56 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; i < 57; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+	return (1);
+}
