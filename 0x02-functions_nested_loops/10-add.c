07e6314553a253075cea2530536d292b0089b01e
root <elafouibadr@gmail.com>
2023-03-21 18:57:22 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @a: First integer we want to sum
+ * @b: Second one
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
