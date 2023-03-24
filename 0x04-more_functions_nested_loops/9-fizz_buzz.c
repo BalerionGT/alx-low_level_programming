89db3a4082d307f83012027d5f42d3f4faf6eff9
root <elafouibadr@gmail.com>
2023-03-23 19:00:23 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ void main(void)
 {
 	int i;
 
-	for (i = 1 ; i <= 100 ; i++)
+	for (i = 1 ; i <= 99 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -31,12 +31,12 @@ void main(void)
 				}
 				else
 				{
-					printf("%d",i);
+					printf("%d", i);
 					printf(" ");
 				}
 			}
 		}
-	printf("\n");
 	}
+	printf("Buzz");
 }
 
