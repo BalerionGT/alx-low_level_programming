d17db1b2909104e0d104035b8994431963e18849
root <elafouibadr@gmail.com>
2023-03-23 18:13:05 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,9 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar(j / 10);
+				_putchar('0' + j / 10);
 			}
-			_putchar(j % 10);
+			_putchar('0' +  j % 10);
 		}
 		_putchar('\n');
 	}
