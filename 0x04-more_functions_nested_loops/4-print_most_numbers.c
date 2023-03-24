d4ded15dad29f3ee060a05f3a744fd3c528cc2c3
root <elafouibadr@gmail.com>
2023-03-23 17:55:17 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Nothing.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57  ; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
