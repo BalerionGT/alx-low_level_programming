b91aaa9faffb4fee60012cc3dcb0d8b962c3adc0
root <elafouibadr@gmail.com>
2023-03-27 19:48:12 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - check the code
+ * @str: a string
+ * Return: Nothing.
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index = length / 2;
+
+	if (length % 2 == 1) 
+	{
+		start_index++;
+	}
+
+	printf("%s\n", str + start_index);
+}
