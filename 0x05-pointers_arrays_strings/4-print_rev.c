e43fcd039a8fa38de9cf0625d77c316b9c96f722
root <elafouibadr@gmail.com>
2023-03-27 18:21:01 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: The caracter we want to print in reverse
+ * Return: Nothing.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	while (i != 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
