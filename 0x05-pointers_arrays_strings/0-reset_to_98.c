e24950449ba2b495aa9da2139d8073ae51cb12ac
root <elafouibadr@gmail.com>
2023-03-27 17:41:31 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - check the code
+ * @n: The pointer that the user will enter.
+ * Return: Nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
