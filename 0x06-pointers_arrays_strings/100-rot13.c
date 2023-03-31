27b4e5e7f0dbefbff56a1d1dd0141713cc053675
root <elafouibadr@gmail.com>
2023-03-30 18:42:47 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+/**
+ * rot13 - Encodes a string using rot13.
+ *
+ * @str: Pointer to the string to encode.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
