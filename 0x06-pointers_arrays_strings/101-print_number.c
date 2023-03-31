461c47c965c896a32da84d07e6cde1f15c05ddb2
root <elafouibadr@gmail.com>
2023-03-30 18:49:27 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer using _putchar.
+ *
+ * @n: The integer to print.
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	int div = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while ((n / div) >= 10)
+	{
+		div *= 10;
+	}
+
+	while (div > 0)
+	{
+		_putchar((n / div) % 10 + '0');
+		div /= 10;
+	}
+}
