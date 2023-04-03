116ca08cf92172b455f53f76c83c6f7fd829d854
root <elafouibadr@gmail.com>
2023-04-03 15:31:19 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _memset - check the code
+ * @s: The space in the memory we want to fill
+ * @b: The caracter we need to fill with
+ * @n: The number of cases we want to fill
+ * Return: Nothing
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	s = (char *) malloc(n * sizeof(char));
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
