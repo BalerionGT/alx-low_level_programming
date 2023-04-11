bda2bbbad00208857a07011a65b7a8b725aed275
root <elafouibadr@gmail.com>
2023-04-10 18:31:51 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[argc - 1]);
+	printf("\n");
+	return (0);
+}
