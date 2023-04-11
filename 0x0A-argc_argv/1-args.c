41a5a6031073f082a7055a2c17958a7429c1b28e
root <elafouibadr@gmail.com>
2023-04-10 18:56:12 -0700

    Task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+(void) (argv); /*ignore argv*/
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
