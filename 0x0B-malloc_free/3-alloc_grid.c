f2e008bb72e3158ece6430aa4a676bd7e1667e2d
root <elafouibadr@gmail.com>
2023-04-12 08:45:05 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,9 +22,8 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < height; i++)
+	if (c == NULL)
 	{
-		if (c == NULL || c[i] == NULL)
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
