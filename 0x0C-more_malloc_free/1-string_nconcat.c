bd99883d8aa3bfd7b31062ee35e3316791f8b598
root <elafouibadr@gmail.com>
2023-04-13 19:47:52 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * string_nconcat - check the code
+ * @s1: the first srting
+ * @s2: the second string
+ * @n: the number of caracters from second string
+ * Return: pointer
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k;
 	char *p;
@@ -16,21 +19,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 		i++;
 	}
 	while (*(s2 + j) != '\0')
-        {
-                j++;
-        }
+	{
+		j++;
+	}
 	if (n >= j)
 	{
-		p = malloc((i + j) * sizeof(char));
+		p = malloc((i + j + 1) * sizeof(char));
+		for (k = 0; k < i ; k++)
+		{
+			p[k] = s1[k];
+		}
+		for (k = i; k < i + j + 1; k++)
+		{
+			p[k] = s2[k];
+		}
+		p[i + j + 1] = '\0';
 	}
 	else
 	{
-		p = malloc((i + n) * sizeof(char));
+		p = malloc((i + n + 1) * sizeof(char));
+		for (k = 0; k < i ; k++)
+		{
+			p[k] = s1[k];
+		}
+		for (k = i; k < i + n + 1; k++)
+		{
+			p[k] = s2[k];
+		}
+		p[i + j + 1] = '\0';
 	}
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	for (k = 0; k < 
-
-
+	return (p);
+}
