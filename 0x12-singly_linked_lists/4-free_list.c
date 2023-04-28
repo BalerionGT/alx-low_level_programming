315db70b6166b4318812639d4a58e5af996a80aa
BalerionGT <elafouibadr@gmail.com>
2023-04-27 19:19:25 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list -check code
+ * @head: the lis we want to free
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *last;
+
+	while (head)
+	{
+		last = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
