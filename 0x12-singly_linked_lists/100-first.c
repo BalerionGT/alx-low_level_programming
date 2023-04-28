785527640612902cf3bf47523b9feb3cb42c473d
BalerionGT <elafouibadr@gmail.com>
2023-04-27 19:55:34 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,11 @@ void first(void)__attribute__((constructor));
 /**
  * first - check code
  * function is executed
+ *
+ * Return: Nothing
  */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("i bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
